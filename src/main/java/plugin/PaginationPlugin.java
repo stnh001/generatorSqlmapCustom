12cbae4809e0fb7925acb0dfca900fcd4226f5ff
luwan <344809958@qq.com>
2018-08-22 14:22:07 +0800

    带分页的逆向工程

---
+++ src/main/java/plugin/PaginationPlugin.java
@@ -0,0 +1,139 @@
+package plugin;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.ShellRunner;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.TextElement;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+
+import java.util.List;
+import java.util.Properties;
+
+public class PaginationPlugin  extends PluginAdapter {
+
+    private FullyQualifiedJavaType serializable;
+    private FullyQualifiedJavaType gwtSerializable;
+    private boolean addGWTInterface;
+    private boolean suppressJavaInterface;
+
+    @Override
+    public void setProperties(Properties properties) {
+        super.setProperties(properties);
+        addGWTInterface = Boolean.valueOf(properties.getProperty("addGWTInterface")); //$NON-NLS-1$
+        suppressJavaInterface = Boolean.valueOf(properties.getProperty("suppressJavaInterface")); //$NON-NLS-1$
+    }
+
+    public PaginationPlugin() {
+        super();
+        serializable = new FullyQualifiedJavaType("java.io.Serializable"); //$NON-NLS-1$
+        gwtSerializable = new FullyQualifiedJavaType("com.google.gwt.user.client.rpc.IsSerializable"); //$NON-NLS-1$
+    }
+
+    /**
+     * 给example添加序列化和分页设置
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        addLimit(topLevelClass, introspectedTable, "limitStart");
+        addLimit(topLevelClass, introspectedTable, "limitEnd");
+        makeSerializable(topLevelClass, introspectedTable);
+        return super.modelExampleClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    /**
+     * 给实体类增加序列化设置
+     *
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        makeSerializable(topLevelClass, introspectedTable);
+        return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        XmlElement isNotNullElement = new XmlElement("if");
+        isNotNullElement.addAttribute(new Attribute("test", "limitStart != null and limitStart >= 0"));
+        isNotNullElement.addElement(new TextElement("limit ${limitStart},${limitEnd}"));
+        element.addElement(isNotNullElement);
+        return super.sqlMapSelectByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    private void makeSerializable(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        if (addGWTInterface) {
+            topLevelClass.addImportedType(gwtSerializable);
+            topLevelClass.addSuperInterface(gwtSerializable);
+        }
+
+        if (!suppressJavaInterface) {
+            topLevelClass.addImportedType(serializable);
+            topLevelClass.addSuperInterface(serializable);
+
+            Field field = new Field();
+            field.setFinal(true);
+            field.setInitializationString("1L");
+            field.setName("serialVersionUID");
+            field.setStatic(true);
+            field.setType(new FullyQualifiedJavaType("long"));
+            field.setVisibility(JavaVisibility.PRIVATE);
+            context.getCommentGenerator().addFieldComment(field, introspectedTable);
+
+            topLevelClass.addField(field);
+        }
+    }
+
+    private void addLimit(TopLevelClass topLevelClass, IntrospectedTable introspectedTable, String name) {
+        CommentGenerator commentGenerator = context.getCommentGenerator();
+        //添加字段
+        Field field = new Field();
+        field.setVisibility(JavaVisibility.PROTECTED);
+        field.setType(FullyQualifiedJavaType.getIntInstance());
+        field.setName(name);
+        field.setInitializationString("-1");
+        commentGenerator.addFieldComment(field, introspectedTable);
+        topLevelClass.addField(field);
+        char c = name.charAt(0);
+        //将首字母转为大写
+        String camel = Character.toUpperCase(c) + name.substring(1);
+        Method method = new Method();
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setName("set" + camel);
+        method.addParameter(new Parameter(FullyQualifiedJavaType.getIntInstance(), name));
+        method.addBodyLine("this." + name + "=" + name + ";");
+        commentGenerator.addGeneralMethodComment(method, introspectedTable);
+        topLevelClass.addMethod(method);
+        method = new Method();
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setReturnType(FullyQualifiedJavaType.getIntInstance());
+        method.setName("get" + camel);
+        method.addBodyLine("return " + name + ";");
+        commentGenerator.addGeneralMethodComment(method, introspectedTable);
+        topLevelClass.addMethod(method);
+    }
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    public static void generate() {
+        String config = PaginationPlugin.class.getClassLoader().getResource("mytatisConfig.xml").getFile();
+        String[] args = {"-configfile", config, "-overwrite"};
+        ShellRunner.main(args);
+    }
+
+    public static void main(String[] args) {
+        generate();
+    }
+
+}
